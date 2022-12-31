b39fcf6257fcfd52e465a29650e8c540b1cece5c
stephrb <stephrb@ad.unc.edu>
2022-12-31 13:08:10 -0500

    text styling for active tab

--- fba-frontend/src/components/layout/MainNavigation.jsx
+++ fba-frontend/src/components/layout/MainNavigation.jsx
@@ -1,32 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 import React from "react";
+
 function MainNavigation() {
+  const location = useLocation();
+
   return (
     <header className={classes.container}>
       <div className={classes.logo}>Fantasy Analytics</div>
       <nav>
         <ul>
           <li>
-            <Link to="">Home</Link>
+            <Link to="/" className={location.pathname === '/' ? classes.active : ''}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/rankings">Power Rankings</Link>
+            <Link to="/rankings" className={location.pathname === '/rankings' ? classes.active : ''}>
+              Power Rankings
+            </Link>
           </li>
           <li>
-            <Link to="/compare">Compare Schedules</Link>
+            <Link to="/compare" className={location.pathname === '/compare' ? classes.active : ''}>
+              Compare Schedules
+            </Link>
           </li>
           <li>
-            <Link to="/playoff">Playoff Machine</Link>
+            <Link to="/playoff" className={location.pathname === '/playoff' ? classes.active : ''}>
+              Playoff Machine
+            </Link>
           </li>
           <li>
-            <Link to="/projections">Matchup Projections</Link>
+            <Link to="/projections" className={location.pathname === '/projections' ? classes.active : ''}>
+              Matchup Projections
+            </Link>
           </li>
           <li>
-            <Link to="/nbagames">NBA Weekly Games</Link>
+            <Link to="/nbagames" className={location.pathname === '/nbagames' ? classes.active : ''}>
+              NBA Weekly Games
+            </Link>
           </li>
           <li>
-            <Link to="/winpercentage">Win Probabilities</Link>
+            <Link to="/winpercentage" className={location.pathname === '/winpercentage' ? classes.active : ''}>
+              Win Probabilities
+            </Link>
           </li>
         </ul>
       </nav>
