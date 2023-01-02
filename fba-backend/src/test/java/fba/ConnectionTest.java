63395b6f26488cd87cac7f284f76d0ad667acccc
stephrb <stephrb@ad.unc.edu>
2023-01-02 18:07:28 -0500

    legacy tests removed

--- fba-backend/src/test/java/fba/ConnectionTest.java
+++ fba-backend/src/test/java/fba/ConnectionTest.java
@@ -18,13 +18,23 @@ public class ConnectionTest {
 
     @Test
     public void testConnection() throws Exception {
-        String sql = "INSERT INTO Errors (league_id, user_id, error_message, error_stacktrace) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO Errors (error_message, error_stacktrace) VALUES (?, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, 1);
-            stmt.setInt(2, 1);
-            stmt.setString(3, "test");
-            stmt.setString(4, "test2");
+            stmt.setString(1, "test");
+            stmt.setString(2, "test2");
             stmt.executeUpdate();
+        }
+    }
+
+    @Test
+    public void numUsersTest() throws Exception {
+        String sql = "SELECT COUNT(DISTINCT user_id, league_id) FROM Logs";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int count = rs.getInt(1);
+                System.out.println("Count: " + count);
+            }
         }
     }
 
